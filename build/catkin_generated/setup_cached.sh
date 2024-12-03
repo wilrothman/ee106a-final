2ef57bceda5bfdcc73acfc1863c643ae71eea543
Class Account <ee106a-aiz@c105-7.local>
2024-12-02 19:24:13 -0800

    deleted unnecessary files

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,5 +7,10 @@
 # new environment variables
 
 # modified environment variables
+export CMAKE_PREFIX_PATH="/home/cc/ee106a/fa24/class/ee106a-aiz/ros_workspaces/final/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/cc/ee106a/fa24/class/ee106a-aiz/ros_workspaces/final/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/cc/ee106a/fa24/class/ee106a-aiz/ros_workspaces/final/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/cc/ee106a/fa24/class/ee106a-aiz/ros_workspaces/final/build'
 export PYTHONPATH="/home/cc/ee106a/fa24/class/ee106a-aiz/ros_workspaces/final/devel/lib/python3/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/cc/ee106a/fa24/class/ee106a-aiz/ros_workspaces/final/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/cc/ee106a/fa24/class/ee106a-aiz/ros_workspaces/final/src:$ROS_PACKAGE_PATH"
