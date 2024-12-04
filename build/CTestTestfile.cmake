a15c91ee1ea9849fc77ac456879e9bbf2da28948
Class Account <ee106a-aiz@c105-7.local>
2024-12-03 17:04:33 -0800

    ready to pull'

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,7 +5,4 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
-subdirs("sawyer_full_stack")
-subdirs("ik")
 subdirs("move_arm")
-subdirs("planning")
